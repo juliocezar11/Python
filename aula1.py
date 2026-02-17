7241c2915893e37eac627e71a8e63aa980318532
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-17 17:47:49 -0300

    Função print e algumas variáveis.

---
+++ aula1.py
@@ -0,0 +1,35 @@
+print("olá, mundo!")
+print(7 + 4)
+print( 'julio cezar' )
+
+#Variáveis ->> recebem um valor
+
+nome = 'julio'
+print(nome)
+idade = '21'
+print(idade)
+peso = '80'
+print(peso)
+print(nome, peso, idade)
+
+nome1 = input('qual o seu nome?')
+idade1 = input('qual sua idade?')
+print(idade1,",", nome1)
+
+
+#desafio 1 
+
+nome = input('Qual é o seu nome?')
+print('Olá', nome,'! Prazer em te conhecer!')
+
+#desafio 2 
+
+dia = input('Dia:')
+mes = input('Mês:')
+ano = input('Ano: ')
+print('Você nasceu no', dia, 'de', mes, 'de', ano, 'correto?')
+
+primeironumero = input('primeiro número:')
+segundonumero = input('segundo número: ')
+soma = int(primeironumero) + int(segundonumero)
+print('A soma é', soma)
