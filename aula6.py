940217ee8cdff0dfdf9ba84910ea6c05d10a0859
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-17 22:02:27 -0300

    Soma

---
+++ aula6.py
@@ -0,0 +1,9 @@
+"""n1 = int(input("Digite um número:"))
+n2 = int(input("Digite outro número:"))
+print("A soma vale", n1 + n2)"""
+
+n1 = int(input("Digite um número:"))
+n2 = int(input("Digite outro número:"))
+s = n1 + n2
+#print("A soma entre", n1 , "e" , n2 ,  "vale",  n1 + n2)
+print("a soma entre {n1} e {n2} vale {s}")
