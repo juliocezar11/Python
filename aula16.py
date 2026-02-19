c6b64f3c70dfc1dd42016900c2f944c5b15b7782
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-19 17:52:22 -0300

    if, elif, else parte 2

--- aula16.py
+++ aula16.py
@@ -2,6 +2,7 @@ entrada = input("Você quer entrar ou sair?")
 
 if entrada == "entrar":
     print("Você entrou no sistema")
+    
     print("1234")
 elif entrada == "sair":
     print("Você saiu no sistema")
---
+++ aula17.py
@@ -0,0 +1,20 @@
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print("Código para a condição 1")
+elif condicao2:
+    print("Código para a condição 2")
+elif condicao3:
+    print("Código para a condição 3")
+elif condicao4:
+    print("Código para a condição 4")
+else:
+    print("Nenhuma Condição foi satisfeita")
+
+if 10 == 10:
+    print("outro if")
+
+print("Fora do If")
