f33c8c02d1392749a5ba0f6facce0b0897b136eb
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-19 17:42:52 -0300

    if, elif, else (condicionais)

---
+++ aula16.py
@@ -0,0 +1,9 @@
+entrada = input("Você quer entrar ou sair?")
+
+if entrada == "entrar":
+    print("Você entrou no sistema")
+    print("1234")
+elif entrada == "sair":
+    print("Você saiu no sistema")
+else:
+    print("Voê não digitou nem entrar e nem sair")
