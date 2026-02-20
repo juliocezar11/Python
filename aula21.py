d460b3d053029ebb573b2b030dbaf50091d13b32
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-20 15:08:14 -0300

    Avaliação de curto circuito

--- aula21.py
+++ aula21.py
@@ -11,17 +11,24 @@
 # entrada = input('[E]ntrar [S]air: ')
 # senha_digitada = input('Senha: ')
 
-
-
-print(True and False and True )
-
-entrada = input('[E]ntrar [S]air')
+'''
+entrada = input('[E]ntrar [S]air: ')
 senha_digitada = input('Senha: ')
+
+
 senha_permitida = '123456'
 
-if entrada == 'E' and senha_digitada == senha_permitida:
+if (entrada == 'E' or entrada =='e') and senha_digitada == senha_permitida:
     print('Entrar')
 else:
     print('Sair')
+'''
+
+
+# Avaliação de Curto circuito
+print(True and 0 and True)
+print(True or False)
+
+
 
 
