5e6ff1511e1091edb5ddc244b1e697796549de8f
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-20 12:14:23 -0300

    Exercício com IF, elif, else e comparação

---
+++ aula20.py
@@ -0,0 +1,11 @@
+primeiro_valor = input('Digite um volor: ')
+segundo_valor = input('Digite outro volor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f' O primeiro valor: {primeiro_valor} é maior que o segundo valor {segundo_valor}')
+elif segundo_valor > primeiro_valor:
+    print(f' O segundo valor: {segundo_valor} é maior que o primeiro valor {primeiro_valor}')
+else:
+    print('Os valores são iguais')
+
+
