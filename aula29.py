6514850b0d1aeb64ea552b8337fa323bb7748163
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-21 19:06:15 -0300

    Try e Except

---
+++ aula29.py
@@ -0,0 +1,26 @@
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+
+numero_str = input('Digite um número: ')
+
+try:
+    print('STR: ', numero_str)
+    print(numero_str.isdigit())
+    numero_float = float(numero_str)
+    print('FLOAT: ', numero_float)
+    print(f'o dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
+
+
+"""
+if numero_str.isdigit():    
+    print(numero_str.isdigit())
+    numero_float = float(numero_str)
+    print(f'o dobro de {numero_str} é {numero_float * 2:.2f}')
+else:
+    print('Isso não é um número')
+"""
