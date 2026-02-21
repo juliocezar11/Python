f1aa4d30b7b75171028b2c82ae8762cc983d6123
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-21 15:03:28 -0300

    fatiamento de str e função len

---
+++ aula27.py
@@ -0,0 +1,27 @@
+# fatiamento de strings
+# 01234567
+# Olá mundo
+#-987654321
+# fatiamento [i f p] [::]
+# a função len retorna a quantidade
+# de caracteres da str
+
+
+
+variavel = 'olá mundo'
+print(variavel[4:])
+print(variavel[0:1])
+print(variavel[1:2])
+print(variavel[2:3])
+print(variavel[3:4])
+print(variavel[4:5])
+print(variavel[5:6])
+print(variavel[6:7])
+print(variavel[7:8])
+print(variavel[8:9])
+print(variavel[9:9])
+print(variavel[0:5])
+print(len(variavel))
+print(variavel[-1:-10:-1])
+
+
