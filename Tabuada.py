6769295f7365648a78084ffc0be60901a3ea6d3b
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-21 22:23:03 -0300

    tabuada
    do 1 ao 10

---
+++ Tabuada.py
@@ -0,0 +1,21 @@
+'''
+
+Peça um número e digite a tabuada do 1 ao 10
+
+'''
+
+num = float(input("Digite um número: "))
+if num:
+    print({num * 1})
+    print({num * 2})
+    print({num * 3})
+    print({num * 4})
+    print({num * 5})
+    print({num * 6})
+    print({num * 7})
+    print({num * 8})
+    print({num * 9})
+    print({num * 10})
+
+else:
+    print('Você não digitou nenhum número')
