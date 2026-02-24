7514ed467a7b957f4162976fedf73419ca195bb0
Julio Cezar <julio.cezar2004@hotmail.com>
2026-02-23 23:57:42 -0300

    Métodos de strings.

---
+++ aula33.py
@@ -0,0 +1,10 @@
+# Imútaveis que vimos; str, int, float, bool
+
+string = 'julio Cezar'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(outra_variavel)
+print(string)
+print(string.capitalize())
+print(string.isdigit())
+print(string.zfill(100))
+
