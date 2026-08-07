f75ca4a512a25c1aae7e1587b5b50995d7ce9db2
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-07 11:39:27 -0300

    math

---
+++ pythonteomewhy/02_calculadora.py
@@ -0,0 +1,22 @@
+# %%
+# soma
+
+print("1 + 1 =", 1 + 1 )
+# %%
+
+# subtração
+print("10 - 4  =", 10 - 4 )
+# %%
+
+# multiplicação
+print("5 * 7  =", 5 * 7 )
+# %%
+# divisão
+print("10 / 5   =", 10 / 5 )
+# divisão inteira parte
+print("10 // 5   =", 10 // 5 )
+# resto da divisão
+print("10 % 4   =", 10 % 4 )
+# potência
+print("3 ** 3    =", 3 ** 3 )
+
