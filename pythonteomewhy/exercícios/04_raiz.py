0225c6c22c9e07c62e534f28ea711571d7d22f06
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-09 11:04:05 -0300

    Create 04_raiz.py

---
+++ pythonteomewhy/exercícios/04_raiz.py
@@ -0,0 +1,9 @@
+# Faça um programa que receba um número inteiro e calcule sua raiz quadrada e exiba o resultado.
+
+numero = input("digite um número inteiro")
+numero = int(numero)
+
+raiz = numero ** (1/2)
+
+
+print("a raíz quadrade de", numero , "é igual a ", raiz )
