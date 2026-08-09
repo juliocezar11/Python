2ca4d73b6bb3855e8132e8619a5e768b9871f28d
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-09 12:17:53 -0300

    Create 05_dobro.py

---
+++ pythonteomewhy/exercícios/05_dobro.py
@@ -0,0 +1,9 @@
+# Faça um programa que exiba o dobro de um número inserido pelo usuário.
+
+numero = input("Digite um número para ver o seu dobro ")
+
+numero = int(numero)
+
+dobro = numero * 2 
+
+print("o dobro de ", numero ,  "é igual a ", dobro)
