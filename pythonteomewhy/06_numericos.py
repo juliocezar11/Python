a2f47ff8b20fa59512f5768c0d5e51019fafc4c7
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-10 20:38:09 -0300

    Create 06_numericos.py

---
+++ pythonteomewhy/06_numericos.py
@@ -0,0 +1,21 @@
+# %%
+
+print("INTEIROS")
+
+print(1)
+print(2+2)
+
+# %%
+print("FLOAT")
+
+print(10/2)
+print(4.0)
+# %%
+print("BOOLEANO")
+
+print(True)
+print(False)
+print(bool(1))
+print(bool(0))
+
+# %%
