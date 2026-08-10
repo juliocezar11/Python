309edc106c4fa843fbeb1580561f187627cd4df6
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-10 20:50:12 -0300

    if

---
+++ pythonteomewhy/08_if.py
@@ -0,0 +1,9 @@
+idade = 16
+
+if idade >= 18:
+    print("você pode beber bebida alcóolica")
+    print("beba com moderação")
+
+if idade < 18:
+    print("você não pode beber bebida alcóolica")
+    print("vá para casa beber leite")
