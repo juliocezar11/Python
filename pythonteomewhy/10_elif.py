a85c13eaab34f8f56b676e3ef1903da6053e5b37
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-10 20:59:28 -0300

    elif

---
+++ pythonteomewhy/10_elif.py
@@ -0,0 +1,20 @@
+#%%
+
+idade = 17
+
+
+if idade >= 67:
+    print("não é recomendado você beber cerveja")
+    print("procure o seu geriatra!")
+
+elif idade >= 18:
+    print("você pode beber bebida alcóolica!")
+    print("beba com moderação")
+
+elif idade == 17:
+    print("você ainda não pode beber bebida alcóolica!")
+    print("beba refrigerante")
+    
+else:
+    print("você não pode beber bebida alcóolica!")
+    print("vá para casa beber leite!")
