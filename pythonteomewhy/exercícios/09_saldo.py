964f2a6e8b40efc7015e2d0241b39837f0383a45
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-12 15:56:49 -0300

    while e break

---
+++ pythonteomewhy/exercícios/09_saldo.py
@@ -0,0 +1,11 @@
+# Faça um programa que receba uma quantidade indefinida de valores correspondentes a “saldo em conta”, mas quando o usuário apertar “enter” sem digitar valor algum, o programa para de receber valores, e exibe a soma de todos os valores digitados anteriormente.
+
+saldo_total = 0 
+
+while True:
+    saldo = input("entre com o saldo: ")
+    if saldo == "":
+        break
+    saldo_total += float(saldo)
+
+print(saldo_total)
