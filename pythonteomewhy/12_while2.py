4b81041334eda5221bd1eb909b2751662040bc13
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-12 15:33:42 -0300

    Update 12_while2.py

--- pythonteomewhy/12_while2.py
+++ pythonteomewhy/12_while2.py
@@ -1,10 +1,12 @@
 #quais numeros são divisivveis por 4 no intervalo [4 - 100]
 # %%
 count = 4 
-while 4 <= 100:
+while count <= 100:
     resto = count % 4 
     if resto == 0:
         print(count)
 
     count += 1 
 
+
+# %%
