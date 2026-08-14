027b63a4d939af2effea609b4e5a7b44c4c54e6e
Julio Cezar <julio.cezar2004@hotmail.com>
2026-08-14 16:26:47 -0300

    listas

---
+++ pythonteomewhy/14_listas.py
@@ -0,0 +1,43 @@
+# %%
+
+# uma mandeira de definir listas 
+idades = [28, 17, 13, 67, 18, 22, 77]
+print(idades)
+
+# %%
+
+julio = ["julio", 22, "namorando", 7777 ]
+print(julio)
+# %%
+# %%
+
+# começa no 0 
+print(julio[1])
+print(julio[0])
+print(julio[3])
+
+# %%
+
+idades = [28, 17, 13, 67, 18, 22, 77, 21, 80]
+print("soma idades: ", sum(idades))
+
+print("qtd idades:", len(idades))
+
+print("media idades: ", sum(idades)/ len(idades))
+
+print("menor idade: ", min(idades))
+
+print("maior idade: ", max(idades))
+
+# %%
+
+
+julio = ["julio", 22, True, "estudante", 7777, ["calculo", "ga", "algelin", "quimica"]]
+print("tamanho do julio", len(julio))
+print(julio[5][0])
+materias = julio[5]
+segunda_materia = materias[1]
+print(segunda_materia)
+
+print(julio[-1][-2])
+# %%
